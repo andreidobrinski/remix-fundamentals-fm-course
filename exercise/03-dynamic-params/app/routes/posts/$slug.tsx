10b8012701e04e53a53c2a015874949ed1589042
Andrei Dobrinski <andreiidobrinski@gmail.com>
2022-10-21 12:03:59 -0400

    3: optimize data send

--- exercise/03-dynamic-params/app/routes/posts/$slug.tsx
+++ exercise/03-dynamic-params/app/routes/posts/$slug.tsx
@@ -17,16 +17,16 @@ export async function loader({ params }: LoaderArgs) {
 
   const html = marked(post.markdown);
 
-  return json({ post, html });
+  return json({ title: post.title, html });
 }
 
 export default function PostRoute() {
-  const { post, html } = useLoaderData<typeof loader>();
+  const { title, html } = useLoaderData<typeof loader>();
 
   return (
     <main className="mx-auto max-w-4xl">
       <h1 className="my-6 border-b-2 text-center text-3xl">
-        {post.title}
+        {title}
       </h1>
       <div dangerouslySetInnerHTML={{ __html: html }} />
     </main>
